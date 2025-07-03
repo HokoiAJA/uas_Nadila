0e4c9c78b3877d9b242db8718f92b5642962be2a
HokoiAJA <mhdabdikc123@gmail.com>
2025-07-03 09:14:51 +0700

    Buat controller ProductController menggunakan –resource.

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProductController;
 
 Route::get('/', function () {
     return view('products');
 });
+
+route::post('/products', [ProductController::class, 'store'])->name('products.store');
